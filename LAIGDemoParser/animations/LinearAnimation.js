39ac135ca5c35a9f6fb866eceb1db7436a643a1f
Couto710 <ei12082@fe.up.pt>
2017-11-26 17:21:54 +0000

    rotation wrking on linear

--- LAIGDemoParser/animations/LinearAnimation.js
+++ LAIGDemoParser/animations/LinearAnimation.js
@@ -64,6 +64,7 @@ class LinearAnimation extends Animation{
 			mat4.identity(this.matrix);
 			mat4.translate(this.matrix, this.matrix, [x, y, z]);
 			mat4.translate(this.matrix, this.matrix, [this.controlPoints[section][0], this.controlPoints[section][1], this.controlPoints[section][2]]);
+			mat4.rotate(this.matrix, this.matrix, Math.atan2(this.controlPoints[section+1][0] - this.controlPoints[section][0], this.controlPoints[section+1][2] - this.controlPoints[section][2]) - Math.PI/2, [0,1,0]);
 		}
 		return this.matrix;
 	}
