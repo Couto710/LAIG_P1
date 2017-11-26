df335f7e90018fb41122d9b52257940d1c42012a
Couto710 <ei12082@fe.up.pt>
2017-11-26 23:40:31 +0000

    tactical commit

--- LAIGDemoParser/animations/BezierAnimation.js
+++ LAIGDemoParser/animations/BezierAnimation.js
@@ -8,5 +8,14 @@ class BezierAnimation extends Animation{
 		super(scene, id);
 		this.controlPoints = controlPoints;
 		this.speed = speed;
+		this.matrix = mat4.create();
+
+		this.p1 = this.controlPoints[0];
+		this.p2 = this.controlPoints[1];
+		this.p3 = this.controlPoints[2];
+		this.p4 = this.controlPoints[3];
+
 	}
+
+	calcMatrix(){}
 }
